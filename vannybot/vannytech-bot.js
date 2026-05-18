605b5a58c1cc76e31ac7d8757b2839d919a65d45
vannyspark25 <babuowino065@gmail.com>
2026-05-19 01:14:45 +0300

    Update vannytech-bot.js

--- vannybot/vannytech-bot.js
+++ vannybot/vannytech-bot.js
@@ -11,6 +11,7 @@ const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
         headless: true,
+        executablePath: '/opt/render/.cache/puppeteer/chrome/linux-146.0.7680.31/chrome-linux64/chrome',
         args: [
             '--no-sandbox',
             '--disable-setuid-sandbox',
